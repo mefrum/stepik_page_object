e5d0ae5267d63ea7cf14b2e87dd8cb7f7a9a5675
Sergey Rumyantsev <rumyantsev.sd93@gmail.com>
2019-06-24 19:35:17 +0300

    Добавлен conftest.py Добавлен test_main_page.py с тестом переход на страницу логина

---
+++ conftest.py
@@ -0,0 +1,34 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+
+
+def pytest_addoption(parser):
+    parser.addoption('--language', action='store', default='en',
+                     help='Choose language')
+    parser.addoption('--browser_name', action='store', default='chrome',
+                     help='Chose browser chrome or firefox')
+
+
+@pytest.fixture
+def browser(request):
+    browser_name = request.config.getoption("browser_name")
+    language = request.config.getoption("language")
+    if browser_name == "chrome":
+        print("\nstart chrome browser for test..")
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': language})
+        browser = webdriver.Chrome(options=options)
+        browser.implicitly_wait(3)
+    elif browser_name == "firefox":
+        print("\nstart firefox browser for test..")
+        fp = webdriver.FirefoxProfile()
+        fp.set_preference("intl.accept_languages", language)
+        browser = webdriver.Firefox(firefox_profile=fp)
+        browser.implicitly_wait(3)
+    else:
+        print("Browser {} still is not implemented".format(browser_name))
+
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
