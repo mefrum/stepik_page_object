09678e2b24b7a1cdd57de1ffe34b0cdc705ee1c3
Sergey Rumyantsev <rumyantsev.sd93@gmail.com>
2019-06-24 21:12:24 +0300

    Добавлены классы с описанием локаторов locators.py Добавлен класс для страницы логина LoginPage Обновлен класс MainPage с использованием класов с локаторами

---
+++ pages/login_page.py
@@ -0,0 +1,23 @@
+from .base_page import BasePage
+from .locators import LoginPageLocators
+
+
+class LoginPage(BasePage):
+    def should_be_login_page(self):
+        self.should_be_login_url()
+        self.should_be_login_form()
+        self.should_be_register_form()
+
+    def should_be_login_url(self):
+        # реализуйте проверку на корректный url адрес
+        actual = self.browser.current_url
+        excepted = "http://selenium1py.pythonanywhere.com/ru/accounts/login/"
+        assert True, "Url is incorrect. \nActual - {} \nExcepted - {} ".format(actual, excepted)
+
+    def should_be_login_form(self):
+        # реализуйте проверку, что есть форма логина
+        assert self.is_element_present(*LoginPageLocators.LOGIN_FORM), "Login form is not present"
+
+    def should_be_register_form(self):
+        # реализуйте проверку, что есть форма регистрации на странице
+        assert self.is_element_present(*LoginPageLocators.REGISTER_FORM), "REGISTER form is not present"
